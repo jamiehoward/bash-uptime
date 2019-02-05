97b46437712c27edbe319a91485e92fe48468385
Jamie Howard <jamiehowardco@gmail.com>
2019-02-05 11:16:19 -0500

    Removed the somehow duplicated code

--- monitor.sh
+++ monitor.sh
@@ -61,26 +61,3 @@ if [ $FAILURES = 0 ]; then
     echo "All servers are up and returning [200] status code."
 fi
 
-# Loop through and check each site
-echo "Checking sites..."
-
-FAILURES=0
-for SITE in $SITES;
-do
-    RESPONSE=`curl -Is $SITE 2>&1 | head -n 1`
-
-    # If there's a failure, let's notify everything
-    if [[ $RESPONSE != *"200"* ]]; then
-        ((FAILURES++))
-        MESSAGE="The server or application at $SITE is down."
-        echo $MESSAGE
-        say "A server or application is down!"
-        curl -X POST -H 'Content-type: application/json' --data "$(generateSlackCall)" https://hooks.slack.com/services/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 > /dev/null 2>&1
-        sleep 
-    fi
-done
-
-# If everything's good, let's say as much
-if [ $FAILURES = 0 ]; then
-    echo "All servers are up and returning [200] status code."
-fi
